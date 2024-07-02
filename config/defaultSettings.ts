6e1531e09e0bba0060c0974baef1984e65b91971
Wenxi Feng <13192677392@163.com>
2024-07-02 16:44:12 +0800

    feat: init add_chart page

--- config/defaultSettings.ts
+++ config/defaultSettings.ts
@@ -15,7 +15,7 @@ const Settings: ProLayoutProps & {
   fixedHeader: false,
   fixSiderbar: true,
   colorWeak: false,
-  title: 'Ant Design Pro',
+  title: 'CCBI',
   pwa: true,
   logo: 'public/logo.svg',
   iconfontUrl: '',
--- config/routes.ts
+++ config/routes.ts
@@ -4,7 +4,8 @@ export default [
     layout: false,
     routes: [{ name: '登录', path: '/user/login', component: './User/Login' }],
   },
-  { path: '/welcome', name: '欢迎', icon: 'smile', component: './Welcome' },
+  { path: '/', redirect: '/add_chart' },
+  { path: '/add_chart', name: 'Add Chart', icon: 'table', component: './AddChart' },
   {
     path: '/admin',
     name: '管理页',
@@ -15,7 +16,6 @@ export default [
       { path: '/admin/sub-page', name: '二级管理页', component: './Admin' },
     ],
   },
-  { name: '查询表格', icon: 'table', path: '/list', component: './TableList' },
-  { path: '/', redirect: '/welcome' },
+  { name: '查询表格', icon: 'smile', path: '/list', component: './TableList' },
   { path: '*', layout: false, component: './404' },
 ];
