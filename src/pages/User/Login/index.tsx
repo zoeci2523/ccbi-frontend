703a5fbad069ade5afe46299bf5c9579f6571534
Wenxi Feng <13192677392@163.com>
2024-07-26 17:54:10 +0800

    feat: change submit button text in login page

--- src/pages/User/Login/index.tsx
+++ src/pages/User/Login/index.tsx
@@ -8,6 +8,7 @@ import { createStyles } from 'antd-style';
 import React, { useState } from 'react';
 import { flushSync } from 'react-dom';
 import Settings from '../../../../config/defaultSettings';
+
 const useStyles = createStyles(({ token }) => {
   return {
     action: {
@@ -104,6 +105,8 @@ const Login: React.FC = () => {
           logo={<img alt="logo" src="/logo.svg" />}
           title="CCBI"
           subTitle={'A Chart Assistant that ease your life'}
+          // 修改登录按钮的文字
+          submitter={{ searchConfig: { submitText: 'Submit' } }}
           onFinish={async (values) => {
             await handleSubmit(values as API.LoginParams);
           }}
@@ -166,4 +169,135 @@ const Login: React.FC = () => {
     </div>
   );
 };
+
+// const Login: React.FC = () => {
+//   // const [userLoginState] = useState<API.LoginResult>({});
+//   const [type, setType] = useState<string>('account');
+//   const { setInitialState } = useModel('@@initialState');
+//   const { styles } = useStyles();
+//   /**
+//    * Get user info after sucessfully sign in
+//    */
+//   const fetchUserInfo = async () => {
+//     const userInfo = await getLoginUserUsingGet();
+//     if (userInfo) {
+//       flushSync(() => {
+//         setInitialState((s) => ({
+//           ...s,
+//           currentUser: userInfo,
+//         }));
+//       });
+//     }
+//   };
+//   const handleSubmit = async (values: API.LoginParams) => {
+//     try {
+//       const res = await userLoginUsingPost(values);
+//       if (res.code === 0) {
+//         const defaultLoginSuccessMessage = 'Successfully login in!';
+//         message.success(defaultLoginSuccessMessage);
+//         await fetchUserInfo();
+//         const urlParams = new URL(window.location.href).searchParams;
+//         history.push(urlParams.get('redirect') || '/');
+//         return;
+//       } else {
+//         message.error(res.message);
+//       }
+//     } catch (error) {
+//       const defaultLoginFailureMessage = 'failed to login in, please try again';
+//       console.log(error);
+//       message.error(defaultLoginFailureMessage);
+//     }
+//   };
+//   // const { status, type: loginType } = userLoginState;
+//   return (
+//     <div className={styles.container}>
+//       <Helmet>
+//         <title>
+//           {'Sign in'}- {Settings.title}
+//         </title>
+//       </Helmet>
+//       {/* <Lang /> */}
+//       <div
+//         style={{
+//           flex: '1',
+//           padding: '32px 0',
+//         }}
+//       >
+//         <Form
+//         name="normal_login"
+//         className="login-form"
+//         initialValues={{ remember: true }}
+//         onFinish={async (values) => {
+//           await handleSubmit(values as API.LoginParams);
+//         }}
+//       >
+//         <Form.Item
+//           name="username"
+//           rules={[{ required: true, message: 'Please input your Username!' }]}
+//         >
+//           <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="ccbi-test" />
+//         </Form.Item>
+//         <Form.Item
+//           name="password"
+//           rules={[{ required: true, message: 'Please input your Password!' }]}
+//         >
+//           <Input
+//             prefix={<LockOutlined className="site-form-item-icon" />}
+//             type="password"
+//             placeholder="ccbi-cool"
+//           />
+//         </Form.Item>
+
+//         <Form.Item>
+//           <Button type="primary" htmlType="submit" className="login-form-button">
+//             Log in
+//           </Button>
+//           Or <a href="">register now!</a>
+//         </Form.Item>
+//       </Form>
+//       </div>
+//       <Footer />
+//     </div>
+//   );
+// };
+
+// const Login: React.FC = () => {
+//   const onFinish = (values: any) => {
+//     console.log('Submited with form of values: ', values);
+//   };
+
+//   return (
+//     <Form
+//       name="normal_login"
+//       className="login-form"
+//       initialValues={{ remember: true }}
+//       onFinish={onFinish}
+//     >
+//       <Form.Item
+//         name="username"
+//         rules={[{ required: true, message: 'Please input your Username!' }]}
+//       >
+//         <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="ccbi-test" />
+//       </Form.Item>
+//       <Form.Item
+//         name="password"
+//         rules={[{ required: true, message: 'Please input your Password!' }]}
+//       >
+//         <Input
+//           prefix={<LockOutlined className="site-form-item-icon" />}
+//           type="password"
+//           placeholder="ccbi-cool"
+//         />
+//       </Form.Item>
+
+//       <Form.Item>
+//         <Button type="primary" htmlType="submit" className="login-form-button">
+//           Log in
+//         </Button>
+//         Or <a href="">register now!</a>
+//       </Form.Item>
+//     </Form>
+//   );
+// };
+
 export default Login;
