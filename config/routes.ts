2abef1d9ed4b24f6d52823bc1558009a3ce1dfc5
Wenxi Feng <13192677392@163.com>
2024-07-26 17:54:43 +0800

    Revert "Reapply "feat: small fix for deploy""
    This reverts commit 871bcfc3c12cae5ab27d1ed59c88c1c6797c607d.

--- config/routes.ts
+++ config/routes.ts
@@ -4,19 +4,19 @@ export default [
     layout: false,
     routes: [{ name: 'login', path: '/user/login', component: './User/Login' }],
   },
-  { path: '/', redirect: '/user/login' },
+  { path: '/', redirect: '/add_chart' },
   { path: '/add_chart', name: 'Add Chart', icon: 'barChart', component: './AddChart' },
   // { path: '/add_chart_sync',name: 'Add Chart Sync', icon: 'barChart', component: './AddChartSync'},
   { path: '/my_chart', name: 'My Chart', icon: 'pieChart', component: './MyChart' },
-  // {
-  //   path: '/admin',
-  //   name: '管理页',
-  //   icon: 'crown',
-  //   access: 'canAdmin',
-  //   routes: [
-  //     { path: '/admin', redirect: '/admin/sub-page' },
-  //     { path: '/admin/sub-page', name: '二级管理页', component: './Admin' },
-  //   ],
-  // },
+  {
+    path: '/admin',
+    name: '管理页',
+    icon: 'crown',
+    access: 'canAdmin',
+    routes: [
+      { path: '/admin', redirect: '/admin/sub-page' },
+      { path: '/admin/sub-page', name: '二级管理页', component: './Admin' },
+    ],
+  },
   { path: '*', layout: false, component: './404' },
 ];
